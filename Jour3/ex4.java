77f5ffab4f1d833bde9a1c01ec833bb27aaac108
||3ry@n <remdji557@gmail.com>
2025-11-09 16:42:35 -0500

    Update ex4.java

--- Jour3/ex4.java
+++ Jour3/ex4.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+
 public class ex4 {
     public static void main(String[] args) {
-        
+        int tbl1[]=new int[5];int max=tbl1[0];int min=tbl1[0];
+        Random random=new Random();
+        for(int i=0;i<tbl1.length;i++){
+            int randomint=random.nextInt(0,100);
+            tbl1[i]= randomint;
+
+        }for(int i=0;i<tbl1.length;i++){
+            if (tbl1[i]>max) {
+                max=tbl1[i];
+            }
+            if (tbl1[1]<min) {
+                min=tbl1[i];
+            }
+        }
+        System.out.println("max ="+max);
+        System.out.println("min ="+min);
     }
 }
