c4a1a41e29a2b2679b8e8ec6809373c9bf6af01f
||3ry@n <remdji557@gmail.com>
2025-11-09 16:08:48 -0500

    ex

--- Jour3/ex3.java
+++ Jour3/ex3.java
@@ -4,20 +4,23 @@ import java.util.Random;
 public class ex3 {
     public static void main(String[] args) {
         Random random=new Random();
-        int randomInt=random.nextInt(0,10);
+        //int randomInt=random.nextInt(0,10); ERREUR il fuat mettre le random dans la boulce pour obtenire plusieurs resultat
         int tblNRM[]=new int[4];
+        int tblRVS[]=new int[4];
+
         for(int i=0;i<tblNRM.length;i++){
+            int randomInt=random.nextInt(0,10);
             tblNRM[i]=randomInt;
         }
-        int tblRVS[]=new int[4];
-        for(int i=tblRVS.length;i<0;i--){
-            tblRVS[i]=randomInt;
+
+        for(int i=0;i<tblNRM.length;i++){//version corriger
+            tblRVS[(tblRVS.length-1)-i]=tblNRM[i];
         }
 
         for(int i=0;i<tblNRM.length;i++){
             System.out.println(" original: "+tblNRM[i]);
-            System.out.println();
-            System.out.print("reversed :"+tblRVS[i]);
+          
+            System.out.println("reversed :"+tblRVS[i]);
         }
     }
 }
