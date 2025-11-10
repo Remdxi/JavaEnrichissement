7797d49235f2d669ae28d0d4655c691d490bd68c
||3ry@n <remdji557@gmail.com>
2025-11-10 15:59:55 -0500

    .

--- Jour3/TP1.java
+++ Jour3/TP1.java
@@ -8,7 +8,7 @@ public class TP1 {
         Scanner scan=new Scanner(System.in);
         //SCANNER POUR LES ENTRER EN  INT
         Scanner sChiffre=new Scanner(System.in);
-        int iChoix; int iPrix;
+        int iChoix; double dPrix;
         //AFFICHAGE
         System.out.println("TRAVAIL PRATIQUE #1 - Bryan Dorceus");
         System.out.println("-----------------------------------");
@@ -19,15 +19,17 @@ public class TP1 {
         System.err.println();
         System.out.println("Choix : ");
         iChoix=scan.nextInt();
-        int iResult=jeuxargent(iPrix);
-        String sResult=String.valueOf(iResult);
-        //BOUCLE DE VÉRIFICATION
         
+        //BOUCLE DE VÉRIFICATION
+        do {
+            System.out.println("Veuiller entrer un nombre valide(1-2-3)");
+            iChoix=sChiffre.nextInt();
+        } while (iChoix<1||iChoix>3);
         switch (iChoix) {
             case 1:
                 System.out.println("Veuiler entrer un prix");
-                iPrix=sChiffre.nextInt();
-                System.out.println(sResult);
+                dPrix=sChiffre.nextInt();
+                System.out.println(jeuxargent(dPrix));
                 break;
             case 2:
                 System.out.println();
@@ -40,11 +42,87 @@ public class TP1 {
         sChiffre.close();
         scan.close();
     }
+/*À partir d’une somme d’argent positive donnée au clavier, vous devez donner le nombre minimum de billets et de pièces. Les billets et pièces disponibles sont 100$, 50$, 20$, 10$, 5$, 2$, 1$, 0,25$, 0,10$, 0,05$ et 0,01$. Vous devez afficher un message qui nous donne le nombre de pièces minimum pour le montant choisi.
+
+Exemple : Si j’ai 188,88$, je reçois en monnaie le montant suivant:
+1x100$, 1x50$, 1x20$, 1x10$, 1x5$, 1x2$, 1x1$, 3x0,25$, 1x0,10$, 3x0,01$.
+ 
+*/
     //MÉTHODE SI sChoix ÉGALE 1
-    public static int jeuxargent(int iPrix) {
+    public static String jeuxargent(double dPrix) {
         double B100=100;double B50=50;double B20=20;double B10=10;double B5=5; double B2=2;int B1=1;double b25=0.25;double b10=0.10;double b5=0.05;double b1=0.01;
-        
-        return;
+        //COMPTEUR DE BILLET
+        int cpt100=0;int cpt50=0;int cpt20=0;int cpt10=0;int cpt5=0;int cpt2=0;int cpt1=0;int cpt025=0;int cpt01=0;int cpt010=0;int cpt005=0;int cpt001=0;
+        //TESTER UNE SEUL BOUCLE WHILE AVEC TOUS LES PRERQUIS
+        //TESTER LA MÉTHOD QUI DEMANDE UN TABLEAU
+        //billet de 100
+        while (dPrix>=B100) {
+            dPrix-=100;
+            cpt100++;
+        }
+        //billet de 50
+        while (dPrix>=B50) {
+            dPrix-=50;
+            cpt50++;
+        }
+        //billet de 20
+        while (dPrix>=B20) {
+            dPrix-=20;
+            cpt20++;
+        }
+        //BILLET DE 10
+        while (dPrix>=B10) {
+            dPrix-=10;
+            cpt10++;
+        }
+        //BILLET DE 5
+        while (dPrix>=B5) {
+            dPrix-=5;
+            cpt5++;
+        }
+        //BILLET DE 2
+        while (dPrix>=B2) {
+            dPrix-=2;
+            cpt2++;
+        }
+        //BILLET DE 1
+        while (dPrix>=B1) {
+            dPrix-=1;
+            cpt1++;
+        }
+        //PIÈCE DE MONNAIES
+        while (dPrix>=b25) {
+            dPrix-=0.25;
+            cpt025++;
+        }
+
+        while (dPrix>=b10) {
+            dPrix-=0.10;
+            cpt010++;
+        }
+
+        while (dPrix>=b5) {
+            dPrix-=0.05;
+            cpt005++;
+        }
+
+        while (dPrix>=b1) {
+            dPrix-=0.01;
+            cpt001++;
+        }
+        String sFin=
+        cpt100+" billet de 100 "+
+        cpt50+" billet de 50 "+
+        cpt20+" billet de 20 "+
+        cpt10+" billet de 10 "+
+        cpt5+" billet de 5 "+
+        cpt2+" billet de 2 "+
+        cpt1+" billet de 1 "+
+        cpt025+" pièces de 0.25 "+
+        cpt010+" pièces de 0.10 "+
+        cpt005+" pièces de 0.05 "+
+        cpt001+" pièces de 0.01 ";
+        return sFin;
     }
     
 }
